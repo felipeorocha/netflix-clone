6b52905b44434c8eeb98e7801e46bc35df136cc9
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-09-10 01:27:49 -0300

    Styles refactor, improve some app styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 
 import apiData from './service/api';
 
--- src/components/categoriesList.js
+++ src/components/categoriesList.js
@@ -4,37 +4,59 @@ import styled, { css } from 'styled-components'
 import size from '../variables';
 
 const CategoriesList = ({ key, title, items }) => {
-  const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
+  const MoviewRow = styled.div`
+    ${props => props.main && css`
+      margin-bottom: 30px;
+    `}
 
-  ${props =>
-    props.primary &&
-    css`
-      background: palevioletred;
-      color: white;
-    `};
-`;
+    ${props => props.listarea && css`
+      overflow-x: hidden;
+      padding-left: 30px;
+    `}
+
+    ${props => props.item && css`
+      display: inline-block;
+      width: 250px;
+
+      & img {
+        width: 100%;
+        transform: scale(0.9);
+        transition: all ease 0.3s;
+        cursor: pointer;
+      }
+
+      & img:hover {
+        transform: scale(1);
+      }
+    `}
+
+    ${props => props.list && css`
+      width: 99999999px;
+    `}
+  `;
+
+  const Title = styled.h2`
+    margin: 0 0 0 30px;
+  `;
 
   return (
-    <>
-      {/* <Button primary>Button</Button> */}
-      <h2>{title}</h2>
-      <div className="movieRow--listarea">
-        { items.results.length > 0 && items.results.map((item, key) => {
-          return (
-            <img
-              src={`https://image.tmdb.org/t/p/${size.small}${item.poster_path}`}
-              alt={item.original_title}
-            />
-          );
-        }) }
-      </div>
-    </>
+    <MoviewRow main>
+      <Title>{title}</Title>
+      <MoviewRow listarea>
+        <MoviewRow list>
+          { items.results.length > 0 && items.results.map((item, key) => {
+            return (
+              <MoviewRow key={key} item>
+                <img
+                  src={`https://image.tmdb.org/t/p/${size.small}${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </MoviewRow>
+            );
+          }) }
+        </MoviewRow>
+      </MoviewRow>
+    </MoviewRow>
   );
 }
 
