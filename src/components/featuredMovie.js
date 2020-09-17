bbc452efa837e2c27fa3ddc4997bba9dc257431f
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-09-17 00:09:15 -0300

    Featured movies component refactor

--- src/components/featuredMovie.js
+++ src/components/featuredMovie.js
@@ -4,37 +4,76 @@ import styled, { css } from 'styled-components'
 import size from '../variables';
 
 const FeaturedMovie = ({ item }) => {
-  const Featured = styled.section`
-    /* ${props => props.main && css`
-      margin-bottom: 30px;
-    `} */
 
+  const FeaturedContainer = styled.section`
     height: 100vh;
     background-size: cover;
     background-position: center;
     background-image: url(https://image.tmdb.org/t/p/${size.original}${item.backdrop_path});
-
-    /* ${props => props.item && css`
-      display: inline-block;
-      width: 250px;
-
-      & img {
-        width: 100%;
-        transform: scale(0.9);
-        transition: all ease 0.3s;
-        cursor: pointer;
-      }
-
-      & img:hover {
-        transform: scale(1);
-      }
-    `} */
   `;
 
+  const VerticalFade = styled.div`
+    width: inherit;
+    height: inherit;
+    background: linear-gradient(to top, #111 10%, transparent 90%);
+  `;
+
+  const HorizontalFade = styled.div`
+    width: inherit;
+    height: inherit;
+    background: linear-gradient(to right, #111 30%, transparent 70%);
+  `;
+
+  const FeaturedName = styled.div`
+
+  `;
+
+  const FeaturedInfo = styled.div`
+
+  `;
+
+  const FeaturedPoints = styled.div`
+
+  `;
+
+  const FeaturedYear = styled.div`
+
+  `;
+
+  const FeaturedSeasons = styled.div`
+
+  `;
+
+  const FeaturedDescription = styled.div`
+
+  `;
+
+  const FeaturedButtons = styled.div`
+
+  `;
+
+  const FeaturedGenres = styled.div`
+
+  `;
+
+  const onAirAt = new Date(item.first_air_date);
+
   return (
-    <Featured>
-      <div>{item.original_name}</div>
-    </Featured>
+    <FeaturedContainer>
+      <VerticalFade>
+        <HorizontalFade>
+          <FeaturedName>{item.original_name}</FeaturedName>
+          <FeaturedInfo>
+            <FeaturedPoints>{item.vote_average} pontos</FeaturedPoints>
+            <FeaturedYear>{onAirAt.getFullYear()}</FeaturedYear>
+            <FeaturedSeasons>{item.number_of_seasons} temporada{ item.number_of_seasons > 1 ? 's' : '' }</FeaturedSeasons>
+          </FeaturedInfo>
+          <FeaturedDescription>{item.overview}</FeaturedDescription>
+          <FeaturedButtons></FeaturedButtons>
+          <FeaturedGenres><strong>Generos: </strong>{item.genres.map(el => el.name).join(', ')}</FeaturedGenres>
+        </HorizontalFade>
+      </VerticalFade>
+    </FeaturedContainer>
   );
 }
 
