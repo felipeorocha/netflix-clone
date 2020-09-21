2b1b9447a69d36ac459ac60004c2977979b5444d
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-09-21 01:53:52 -0300

    Footer refactor: bottom fixed

--- src/appStyles.js
+++ src/appStyles.js
@@ -8,4 +8,8 @@ export const Footer = styled.footer`
   margin: 30px 0;
   color: #999;
   text-align: center;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  position: ${({loader}) => loader ? "absolute" : "relative"};
 `;
